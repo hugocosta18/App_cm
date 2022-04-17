9cbaae4da7d93efd0ba8f24e371a06b7cdf7be85
hugocosta18 <82047791+hugocosta18@users.noreply.github.com>
2022-04-17 17:18:05 +0100

    Ciclo de vida

--- app/src/main/java/ipvc/estg/app_cm/MainActivity.kt
+++ app/src/main/java/ipvc/estg/app_cm/MainActivity.kt
@@ -2,10 +2,39 @@ package ipvc.estg.app_cm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("****TAG","onCreate")
+
     }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("****TAG","onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d("****TAG","onPause")
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.d("****TAG","onStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d("****TAG","onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("****TAG","onDestroy")
+    }
+
+
+
+
 }
